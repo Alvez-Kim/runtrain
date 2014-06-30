6cc5bdd100a39a3cd8fadb5c04d680d7aac4099f
rx_jinjunjie <sweet_dreams@aliyun.com>
2014-06-30 19:07:01 +0800

    quartz integration with spring

---
+++ src/main/java/pac/king/schedule/MinutePrintJob.java
@@ -0,0 +1,31 @@
+package pac.king.schedule;
+
+import java.util.Date;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Service;
+
+@Service("minPrintJob")
+public class MinutePrintJob extends QuartzJobBean {
+	
+	private int timeout;
+	public void setTimeout(int timeout) {
+		this.timeout = timeout;
+	}
+
+	@Override
+	protected void executeInternal(JobExecutionContext context)
+			throws JobExecutionException {
+		logger.info("Current Time:::"+new Date()+"\t timeout::"+timeout++);
+	}
+	
+	public void justDoIt(){
+		logger.info("Kim say:just do it!!");
+	}
+
+	static final Logger logger = LoggerFactory.getLogger(MinutePrintJob.class);
+}
