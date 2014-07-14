ed660291417e91b8517b2b8d808fb7efe2f3eccb
rx_jinjunjie <sweet_dreams@aliyun.com>
2014-07-14 20:28:15 +0800

    test rabbit rpc

---
+++ src/test/java/pac/testcase/jms/rabbit/TestRabbitRpc.java
@@ -0,0 +1,111 @@
+package pac.testcase.jms.rabbit;
+
+
+import com.rabbitmq.client.*;
+import com.rabbitmq.client.AMQP.BasicProperties;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by Administrator on 14-7-14.
+ */
+public class TestRabbitRpc {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class RPCServer{
+    private static final String RPC_QUEUE_NAME = "rpc_queue";
+    public static void main(String[] args) throws Exception {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(RPC_QUEUE_NAME, false, false, false, null);
+
+        channel.basicQos(1);
+
+        QueueingConsumer consumer = new QueueingConsumer(channel);
+        channel.basicConsume(RPC_QUEUE_NAME, false, consumer);
+
+        System.out.println(" [x] Awaiting RPC requests");
+
+        while (true) {
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+
+            BasicProperties props = delivery.getProperties();
+            BasicProperties replyProps = new BasicProperties
+                    .Builder()
+                    .correlationId(props.getCorrelationId())
+                    .build();
+
+            String message = new String(delivery.getBody());
+            int n = Integer.parseInt(message);
+
+            System.out.println(" [.] fib(" + message + ")");
+            String response = "" + fib(n);
+
+            channel.basicPublish( "", props.getReplyTo(), replyProps, response.getBytes());
+
+            channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+        }
+
+    }
+
+    private static int fib(int n) throws Exception {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fib(n-1) + fib(n-2);
+    }
+}
+
+class RPCClient{
+
+    private Connection connection;
+    private Channel channel;
+    private String requestQueueName = "rpc_queue";
+    private String replyQueueName;
+    private QueueingConsumer consumer;
+
+    public RPCClient() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+
+        replyQueueName = channel.queueDeclare().getQueue();
+        consumer = new QueueingConsumer(channel);
+        channel.basicConsume(replyQueueName, true, consumer);
+    }
+
+    public String call(String message) throws Exception {
+        String response = null;
+        String corrId = java.util.UUID.randomUUID().toString();
+
+        BasicProperties props = new BasicProperties
+                .Builder()
+                .correlationId(corrId)
+                .replyTo(replyQueueName)
+                .build();
+
+        channel.basicPublish("", requestQueueName, props, message.getBytes());
+
+        while (true) {
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            if (delivery.getProperties().getCorrelationId().equals(corrId)) {
+                response = new String(delivery.getBody());
+                break;
+            }
+        }
+
+        return response;
+    }
+
+    public void close() throws Exception {
+        connection.close();
+    }
+}
