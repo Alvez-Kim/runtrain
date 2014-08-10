2b7a5e1831b228c29fb00c7bd67d59bb263a8d0b
rx_jinjunjie <sweet_dreams@aliyun.com>
2014-08-10 19:39:41 +0800

    remove deprecated

--- src/test/java/pac/testcase/basic/thread/Observable.java
+++ src/test/java/pac/testcase/basic/thread/Observable.java
@@ -1,16 +1,76 @@
 package pac.testcase.basic.thread;
 
 import java.util.*;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by Alvez on 14-7-26.
  */
 public class Observable {
 
-	public static void main(String[] args) throws InterruptedException {
+		public static void main(String[] args) {
+			ObservableSet<Integer> set = new ObservableSet<Integer>(
+					new HashSet<Integer>());
+
+			// Observer that uses a background thread needlessly
+			set.addObserver(new SetObserver<Integer>() {
+				public void added(final ObservableSet<Integer> s, Integer e) {
+					System.out.println(e);
+					if (e == 23) {
+						ExecutorService executor = Executors
+								.newSingleThreadExecutor();
+						final SetObserver<Integer> observer = this;
+						try {
+							executor.submit(new Runnable() {
+								public void run() {
+									s.removeObserver(observer);
+								}
+							}).get();
+						} catch (ExecutionException ex) {
+							throw new AssertionError(ex.getCause());
+						} catch (InterruptedException ex) {
+							throw new AssertionError(ex.getCause());
+						} finally {
+							executor.shutdown();
+						}
+						
+/*						Thread t = new Thread(new Runnable() {
+							
+							
+							@Override
+							public void run() {
+								s.removeObserver(observer);
+								
+							}
+						});
+						t.start();*/
+					}
+				}
+			});
+
+			for (int i = 0; i < 100; i++)
+				set.add(i);
+		}
 		
-	}
+
+		/*public static void main(String[] args) {
+			ObservableSet<Integer> set = new ObservableSet<Integer>(
+					new HashSet<Integer>());
+
+			set.addObserver(new SetObserver<Integer>() {
+				public void added(ObservableSet<Integer> s, Integer e) {
+					System.out.println(e);
+					if (e == 23)
+						s.removeObserver(this);
+				}
+			});
+
+			for (int i = 0; i < 100; i++)
+				set.add(i);
+		}*/
+
 	
 }
 
