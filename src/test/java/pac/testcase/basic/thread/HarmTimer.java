6927ac200983ee4423b59a9840a1886f897f0749
rx_jinjunjie <sweet_dreams@aliyun.com>
2014-10-21 20:36:00 +0800

    harm timer

---
+++ src/test/java/pac/testcase/basic/thread/HarmTimer.java
@@ -0,0 +1,48 @@
+package pac.testcase.basic.thread;
+
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class HarmTimer {
+    /**
+     * even if i catched runtimeexception , tasks after first task just failed
+     * @throws InterruptedException
+     */
+    @Test
+    public void go() throws InterruptedException {
+        Timer timer = new Timer("timer-1",false);
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                try{
+                    //throw new RuntimeException();
+                }catch (Exception e){
+                    System.out.println("catched");
+                }
+            }
+        };
+        timer.schedule(task,1000);
+        Thread.currentThread().sleep(1000l);
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("task 2");
+            }
+        },1000);
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("task 3");
+            }
+        },1000);
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("task 4");
+            }
+        },1000);
+    }
+}
